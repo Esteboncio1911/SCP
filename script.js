8d4719a4df98965a4446e885de8ca747cefc33b5
Esteboncio1911 <arielrufa19@gmail.com>
2025-02-21 16:40:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -51,6 +51,48 @@ const scps = [
                 description: "Experimento 049-T1: Conversión de D-Class"
             }
         ]
+    },
+    {
+        id: "1002",
+        name: "████████",
+        class: "Apollyon",
+        level: "5",
+        preview: "Algunos lo describen como una criatura del inframundo, otros como un angel...",
+        fullDescription: "El Scp 1002 “Oscuro Ángel” es una criatura paralela proveniente de otra dimension, cuya aparicion surgió despues de que un portal interdimensional se abriera en 	el norte.
+	Dias despues de su cierre, se detectaron rasgos de inundaciones o avalanchas nunca antes vistas.
+	Cuando empeoro todo, un equipo E-11 se dirigio hacia el lugar a hacer la expedicion.
+	En la busqueda, se detectaron signos de vida cerca, ya que habian huellas tapadas por la nieve.
+	Casi al limite de cancelar la busqueda, una avalancha cayó encima del escuadrón, un sonido extraño aullaba y parecia decir conjuros o algun tipo de magia.
+
+	Las cámaras del escuadrón quedaron inutilizables por los golpes y no hubo ni rastros del escuadrón el cual desapareció despues de la avalancha.
+	El Consejo estaba enfadado, era una amenaza grave, cuyo poder no puede pertenecer a este lugar. El caso quedó en busqueda durante un largo periodo. . .
+
+	2 Años despues. . .
+
+	El Dr. Michael fue encomendado a buscar unos documentos de 999, cuando entro en una sala que nunca habia visto antes miró en el cajon clasificado en keter, y encontró unos papeles 	antiguos y arrugados.
+	En esos documentos, habian evidencias de un Scp denominado 1002. Un hechicero o mago con mucho poder que supuestamente escapó de las instalaciones hace mucho tiempo.
+	
+	Su descripcion era tal que asi: Una criatura blanca con unas extrañas figuras y letras que parecian tener un conjuro en ellas, consigo llevaba un baston con una piedra que 	brillaba.
+	En su cintura llevaba dos espadas de fuego negro. Tenia una cola blanca e intimidante y una capucha que oscurocia su cara. 
+	Era alto y fuerte, con multiples hechizos que casi nadie conocia. Y siempre llevaba un cuchillo pequeño, con letras escritas y un color gris.
+	Esta anomala criatura fue capturada en 1931, cuando estaba en su cueva con su padre, salio para buscar algo que cenar.
+	Cuando salio, se encontro con algo similar a una criatura de su planeta y rapidamente se acerco hacia ella.
+	Justo apunto de cazarla, el mismo cayo en su propia trampa y fue encerrado por redes y barras de metal.
+	Su padre, cuando le parecio raro su tardanza, salio a buscarlo, y solo encontro su amuleto tirado junto con sangre en los alrededores.
+	
+	Unos años despues, el scp 1002 escapo y cuando encontro a su padre estaba muerto, no tenia alimento y murio en su busqueda.
+	
+	Le recordo a su madre, la cual murio protegiondolos de unos lobos hambrientos que entraron a su hogar.",
+        containment: "El SCP-1002 debe ser contenido en una celda de 100x100 metros, con una altura de 5 metros. El SCP-1002 debé creer que se encuentra en un lugar natural y arbolado.
+	La comida será proporcionada de una manera discreta, dejando a este SCP cazar.",
+        image: "scp1002.jpg",
+        procedures: "No se permite el contacto directo del personal con este SCP.",
+        incidents: [
+            {
+                date: "██/██/████",
+                description: "████████████████████████████████████"
+            }
+        ]
     }
 ];
 
